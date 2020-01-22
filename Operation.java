db5952f148e0e6acaffba0d90f3314aa2d81cadc
Alvaro012 <aldedieg@ucm.es>
2020-01-22 12:27:39 +0100

    Fifth commit

---
+++ Operation.java
@@ -0,0 +1,8 @@
+public class ArithmeticDemo {
+    public static void main(String[] args) {
+      int i = 1;
+      int j = 1;
+      System.out.println("sol= "i+j);
+      System.out.println("Done");
+    }
+}
